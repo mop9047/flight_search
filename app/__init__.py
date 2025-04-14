cab91e1c5183c560aa4c4e9d3874e105f2b28f0f
Mathew Ponon <mop9047@nyu.edu>
2025-04-14 16:14:05 -0400

    refactored code to separate files

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+def create_app():
+    #Initialize the app from Flask
+    app = Flask(__name__)
+
+    app.secret_key = 'some key that you will never guess'
+    
+    #Configure MySQL
+    app.config['db'] = pymysql.connect(host='localhost',
+                        user='root',
+                        password='root',
+                        unix_socket='/Applications/MAMP/tmp/mysql/mysql.sock',
+                        database='blog',
+                        charset='utf8mb4',
+                        cursorclass=pymysql.cursors.DictCursor)
+
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    return app
+
