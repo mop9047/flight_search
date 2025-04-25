334deefd0586f0e343a64954c0af6681925b9fcd
Mathew Ponon <mop9047@nyu.edu>
2025-04-25 15:19:38 -0400

    added logic for extra phone numbers and using the phone tables

--- app/blueprints/auth/authStaff.py
+++ app/blueprints/auth/authStaff.py
@@ -46,14 +46,26 @@ def loginAuthStaff():
 #Authenticates the register of Staff
 @auth.route('/registerAuthAirlineStaff', methods=['GET', 'POST'])
 def registerAuthStaff():
+    print(request.form)
     #grabs information from the forms
     username = request.form['username']
     password = request.form['password']
     f_name = request.form['f_name']
     l_name = request.form['l_name']
     date_of_birth = request.form['date_of_birth']
+    phone_nums = request.form.getlist('phone[]')
+    email_add = request.form.getlist('email[]')
     airline_name = request.form['airline_name']
 
+
+    phone_set = set(phone_nums)
+    email_set = set(email_add)
+
+    phones = list(phone_set)
+    emails = list(email_set)
+
+    print(phones)
+    print(emails)
     # Format the date to YYYY-MM-DD if it's in MM-DD-YYYY format
     try:
         if '-' in date_of_birth:
@@ -79,6 +91,16 @@ def registerAuthStaff():
     else:
         ins = 'INSERT INTO Airline_Staff VALUES(%s, md5(%s),%s,%s,%s,%s)'  #added md5 to hash password
         cursor.execute(ins, (username, password, f_name, l_name, date_of_birth, airline_name))
+
+        print('PHONES',phones,'EMAILS',emails)
+        for phone in phones:
+            query1 = 'INSERT INTO Phone_num VALUES(%s, %s)'  #added md5 to hash password
+            cursor.execute(query1, (username, phone))
+        
+        for email in emails:
+            query2 = 'INSERT INTO Email VALUES(%s, %s)'  #added md5 to hash password
+            cursor.execute(query2, (username, email))
+        
         current_app.config['db'].commit()
         cursor.close()
         return render_template('index.html')
