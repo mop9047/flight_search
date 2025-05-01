58e1c0b27a3dcbbbcc867b62ce03158db3f20dac
Mathew Ponon <mop9047@nyu.edu>
2025-05-01 12:20:07 -0400

    added logic for create flight to be edited if the value is repeated, some code clean up

--- app/blueprints/auth/authStaff.py
+++ app/blueprints/auth/authStaff.py
@@ -16,10 +16,6 @@ def loginAuthStaff():
     cursor.execute(query, (username, password))
 	#stores the results in a variable
     data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-    query = 'SELECT * FROM Airline_Staff WHERE username = %s and password = md5(%s)' #md5 to hash password
-    cursor.execute(query, (username, password))
-    
     cursor.close()
     
     error = None
@@ -35,7 +31,6 @@ def loginAuthStaff():
         cursor.close()
 
         session['airline'] = airline['Airline_name']
-        print(session)
 
         return redirect(url_for('main.home'))
     else:
@@ -64,8 +59,6 @@ def registerAuthStaff():
     phones = list(phone_set)
     emails = list(email_set)
 
-    print(phones)
-    print(emails)
     # Format the date to YYYY-MM-DD if it's in MM-DD-YYYY format
     try:
         if '-' in date_of_birth:
@@ -92,13 +85,12 @@ def registerAuthStaff():
         ins = 'INSERT INTO Airline_Staff VALUES(%s, md5(%s),%s,%s,%s,%s)'  #added md5 to hash password
         cursor.execute(ins, (username, password, f_name, l_name, date_of_birth, airline_name))
 
-        print('PHONES',phones,'EMAILS',emails)
         for phone in phones:
-            query1 = 'INSERT INTO Phone_num VALUES(%s, %s)'  #added md5 to hash password
+            query1 = 'INSERT INTO Phone_num VALUES(%s, %s)'
             cursor.execute(query1, (username, phone))
         
         for email in emails:
-            query2 = 'INSERT INTO Email VALUES(%s, %s)'  #added md5 to hash password
+            query2 = 'INSERT INTO Email VALUES(%s, %s)' 
             cursor.execute(query2, (username, email))
         
         current_app.config['db'].commit()
